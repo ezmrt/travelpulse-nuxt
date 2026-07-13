2124b3817b6e91944e4f4cec73fe5a37fa9be566
ezmrt <zamaratskaya.eliza@gmail.com>
2026-07-13 15:42:26 +0800

    Update PopularTours.vue
    Прописала пути к Популярным Турам

--- components/PopularTours.vue
+++ components/PopularTours.vue
@@ -28,12 +28,12 @@ export default {
   data() {
     return {
       tours: [
-        { id: 1, title: 'France, Paris', description: 'DisneyLand - экскурсия, питание, отель.', rating: 4.7, image: '' },
-        { id: 2, title: 'Italy, Roma', description: 'Coliseum - экскурсия, питание, отель.', rating: 4.2, image: '' },
-        { id: 3, title: 'Indonesia, Bali', description: 'Tanakh-Lot - экскурсия, питание, отель.', rating: 5.0, image: '' },
-        { id: 4, title: 'Russia, Irkutsk', description: 'Olkhon - экскурсия, питание, отель.', rating: 4.0, image: '' },
-        { id: 5, title: 'China', description: 'The Great Wall of China - экскурсия, питание, отель.', rating: 4.1, image: '' },
-        { id: 6, title: 'UAE, Dubai', description: 'Burj Khalifa - экскурсия, питание, отель.', rating: 4.8, image: '' }
+        { id: 1, title: 'France, Paris', description: 'DisneyLand - экскурсия, питание, отель.', rating: 4.7, image: '/images/7a40393a512a51cca6106fff2ec2f6d346c82e52.jpg' },
+        { id: 2, title: 'Italy, Roma', description: 'Coliseum - экскурсия, питание, отель.', rating: 4.2, image: '/images/98aa2ab88e094d8632cbbd8389a31455d15c8f9c.jpg' },
+        { id: 3, title: 'Indonesia, Bali', description: 'Tanakh-Lot - экскурсия, питание, отель.', rating: 5.0, image: '/images/582c258e257bf46eb30918e5154a5e8f714f279b.jpg' },
+        { id: 4, title: 'Russia, Irkutsk', description: 'Olkhon - экскурсия, питание, отель.', rating: 4.0, image: '/images/bb9b0c207f49d1a2c98168429318321bac7c0fe9.jpg' },
+        { id: 5, title: 'China', description: 'The Great Wall of China - экскурсия, питание, отель.', rating: 4.1, image: '/images/9651db3182b7ca0bb6dcb92675e163b3ce3ed50f.jpg' },
+        { id: 6, title: 'UAE, Dubai', description: 'Burj Khalifa - экскурсия, питание, отель.', rating: 4.8, image: '/images/12f7d81e6b792fb294ede1d72a9e69f6708bbf89.jpg' }
       ]
     }
   }
